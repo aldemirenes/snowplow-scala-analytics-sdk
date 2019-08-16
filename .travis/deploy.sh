c2feba254401169a328a0319b282f41a7ad0b6de
Enes Aldemir <aldemirenes@live.com>
2019-08-16 14:04:14 +0300

    Integrate sbt-gh-pages to create GH Pages from Scaladoc

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -18,8 +18,9 @@ pwd
 
 project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print $1 if /(\d+\.\d+\.\d+[^\r\n]*)/')
 if [ "${project_version}" == "${tag_version}" ]; then
-    sbt +publish
-    sbt +bintraySyncMavenCentral
+    ./deploy_docs.sh
+    #sbt +publish
+    #sbt +bintraySyncMavenCentral
 else
     echo "Tag version '${tag_version}' doesn't match version in scala project ('${project_version}'). Aborting!"
     exit 1
